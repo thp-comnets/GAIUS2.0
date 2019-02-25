00e8dc235e96629bd8cbb3d83fb6784c8e3f1028
thp-comnets <thp@comnets.uni-bremen.de>
2019-02-25 10:58:55 +0400

    improved layout in login activity

--- app/src/main/java/com/gaius/gaiusapp/LoginActivity.java
+++ app/src/main/java/com/gaius/gaiusapp/LoginActivity.java
@@ -37,8 +37,6 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // try to login first with saved/cached email and password
-//        loginFromSaveData();
 
         setContentView(R.layout.login_activity);
         super.onCreate(savedInstanceState);
